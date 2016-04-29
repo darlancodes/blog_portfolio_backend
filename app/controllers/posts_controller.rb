a59eff86c05cb92100ad83bdcfff2979100f428e
DarlanRic <darlanric@live.com>
2016-04-28 22:56:48 -0300

    Posts CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+
+  #Before anything it is going to find the post
+  before_action :find_post, only: [:show, :edit,:update,:destroy]
+  def index
+  end
+
+  #Creation of new posts inside form
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    #If the post saves correctly....
+    if @post.save
+      redirect_to @post, notice: "Congrats Darlan! Successfully saved :)"
+    else
+      render 'new', notice: "Oh No... Not able to save the article :("
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def update
+    if @post.update post_params #Just put the name of the method after the method of updating database, fo fill in the parameters
+      redirect_to @post, notice:  "Article saved Buddy !"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+
+end
