4f0fd36700ff7ee79af1be0905647cb43cff13f5
DarlanRic <darlanric@live.com>
2016-05-03 08:21:49 -0300

    Additional Styling for index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @posts = Post.all.limit(3).order("created_at desc")
+    #limita a 3 posts com a ordem em data de criação
   end
 end
